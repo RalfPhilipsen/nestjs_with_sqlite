37b713e719adba9f0a3d6a4dd4bd6c657ea04029
Ralf Philipsen <ralf.philipsen@pttrns.ai>
2024-12-17 18:27:21 +0100

    Add category entity and table

---
+++ src/db/migrations/1734455882570-CreateCategories.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateCategories1734455882570 implements MigrationInterface {
+    name = 'CreateCategories1734455882570';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE "category" (
+              "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+              "name" varchar NOT NULL,
+              "created_at" datetime NOT NULL DEFAULT (datetime('now')),
+              "updated_at" datetime NOT NULL DEFAULT (datetime('now')),
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "category"`);
+    }
+}
